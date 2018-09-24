d4f7b07eb5a4993323d4b328638a989ef65e6b88
Jonas Gliss <jonasgliss@gmail.com>
2018-09-24 14:33:25 +0200

    Added helpers.py module

---
+++ helpers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Helper methods used in notebooks
+"""
+
+def dataframe_highlight_4d_no_altitude_dim(x, bg_color='#ffcccc'):
+    #copy df to new - original data are not changed
+    df = x.copy()
+    #set by condition
+    m1 = df['Dim'] == 4
+    m2 = 'altitude' not in df['Dim names']
+    mask = m1 * m2
+    df.loc[mask, :] = 'background-color: {}'.format(bg_color)
+    df.loc[~mask,:] = 'background-color: ""'
+    return df   
